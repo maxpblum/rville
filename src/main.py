b3c2e30f6059cf365b654b4665d19cadfc5e5c62
Max Blum-Campo <maxpblum@users.noreply.github.com>
2024-07-28 09:33:15 +0000

    Working well!

--- src/main.py
+++ src/main.py
@@ -41,32 +41,17 @@ ALL_TIME_SLOTS: list[TimeSlot] = [
 Gender = Literal["M", "W"]
 ALL_GENDERS: list[Gender] = ["M", "W"]
 
+Player = collections.namedtuple('Player', ['gender', 'id'])
 
-@dataclass(frozen=True)
-class Player:
-    gender: Gender
-    id: int
-
-
-def make_pair(p1: Player, p2: Player) -> frozenset[int]:
-    return frozenset([p1.id, p2.id])
+Assignment = collections.namedtuple('Assignment',
+                                    ['time_slot', 'men', 'women'])
 
 
-@dataclass(frozen=True)
-class Assignment:
-    time_slot: TimeSlot
-    court: int
-    men: frozenset[int]
-    women: frozenset[int]
-
-    def player_present(self, p: Player):
-        return p.id in (self.men if p.gender == "M" else self.women)
+def player_present(a: Assignment, p: Player):
+    return p.id in (a.men if p.gender == "M" else a.women)
 
 
-@dataclass(frozen=True)
-class AssignmentVar:
-    a: Assignment
-    v: cp_model.IntVar
+AssignmentVar = collections.namedtuple('AssignmentVar', ['a', 'v'])
 
 
 def solve(
@@ -74,67 +59,81 @@ def solve(
     women_count: int,
     courts_count: int,
     matches_count: int,
+    accommodate_blampos: bool,
 ) -> tuple[cp_model_pb2.CpSolverStatus, list[Assignment]]:
     model = cp_model.CpModel()
 
     men = [Player(gender="M", id=id) for id in range(1, men_count + 1)]
+    max_player = men[3]
+    max_time_slots = ['11am', '12pm', '2pm']
     women = [Player(gender="W", id=id) for id in range(1, women_count + 1)]
+    gigi_player = women[5]
+    gigi_time_slots = ['9am', '10am', '1pm']
     all_players: list[Player] = men + women
-    courts = range(1, courts_count + 1)
 
-    # A mapping from time slot to list of courts.
-    settings: dict[TimeSlot, list[int]] = collections.defaultdict(list)
-    for _, (time_slot, court) in zip(range(matches_count),
-                                     itertools.product(ALL_TIME_SLOTS,
-                                                       courts)):
-        settings[time_slot].append(court)
+    # A mapping from time slot to number of courts.
+    settings: dict[TimeSlot, int] = collections.Counter()
+    for time_slot in ALL_TIME_SLOTS:
+        settings.update({time_slot: min(matches_count, courts_count)})
+        matches_count -= settings[time_slot]
+        if matches_count == 0:
+            break
+
     time_slots = list(settings.keys())
-
-    def iter_settings() -> Iterable[tuple[TimeSlot, int]]:
-        for time_slot, courts in settings.items():
-            for court in courts:
-                yield time_slot, court
 
     assignments: list[AssignmentVar] = []
 
-    for (time_slot, court), men_pair, women_pair in itertools.product(
-            iter_settings(),
+    for time_slot, men_pair, women_pair in itertools.product(
+            time_slots,
             itertools.combinations(men, 2),
             itertools.combinations(women, 2),
     ):
-        assignment = Assignment(time_slot, court, make_pair(*men_pair),
-                                make_pair(*women_pair))
+        # Special cases to help Max and Gigi get parenting done.
+        if accommodate_blampos and max_player in men_pair and time_slot not in max_time_slots:
+            continue
+        if accommodate_blampos and gigi_player in women_pair and time_slot not in gigi_time_slots:
+            continue
+
+        assignment = Assignment(time_slot=time_slot,
+                                men=(men_pair[0].id, men_pair[1].id),
+                                women=(women_pair[0].id, women_pair[1].id))
         var = model.NewBoolVar(repr(assignment))
         assignments.append(AssignmentVar(a=assignment, v=var))
 
-    # Every timeslot x court should have exactly one assigned group.
-    for time_slot, court in iter_settings():
+    # Every court should have at most one match per time slot.
+    for (time_slot, ts_courts_count) in settings.items():
         model.add(
             sum(a.v for a in assignments
-                if a.a.time_slot == time_slot and a.a.court == court) == 1)
+                if a.a.time_slot == time_slot) <= ts_courts_count)
 
     # Players should have three or four matches.
     for p in men + women:
-        player_match_count = sum(a.v for a in assignments
-                                 if a.a.player_present(p))
-        model.add(player_match_count >= 3)
-        model.add(player_match_count <= 4)
+        model.add(sum(a.v for a in assignments if player_present(a.a, p)) >= 3)
+        model.add(sum(a.v for a in assignments if player_present(a.a, p)) <= 4)
 
     # No two players should be in the same match twice.
+    for pa, pb in itertools.combinations(all_players, 2):
+        model.add(
+            sum(a.v for a in assignments
+                if player_present(a.a, pa) and player_present(a.a, pb)) <= 1)
 
     # Every player is in at most one match per time slot.
     for p, time_slot in itertools.product(all_players, time_slots):
         model.add(
             sum(a.v for a in assignments
-                if a.a.player_present(p) and a.a.time_slot == time_slot) <= 1)
+                if player_present(a.a, p) and a.a.time_slot == time_slot) <= 1)
 
     # No player should play three matches in a row.
-    # time_streaks = [time_slots[i : i + 3] for i in range(len(time_slots) - 2)]
-    # for p, streak in itertools.product(men + women, time_streaks):
-    #  pass
+    time_streaks = [time_slots[i:i + 3] for i in range(len(time_slots) - 2)]
+    for p, streak in itertools.product(all_players, time_streaks):
+        model.add(
+            sum(a.v for a in assignments
+                if player_present(a.a, p) and a.a.time_slot in streak) < 3)
 
+    print('Solving')
     solver = cp_model.CpSolver()
     status = solver.Solve(model)
+    print(f'Status: {solver.StatusName(status)}')
 
     final_assignments: list[Assignment] = (
         [a.a for a in assignments
@@ -149,25 +148,41 @@ def generate_csv(
     women_count: int,
     courts_count: int,
     matches_count: int,
+    accommodate_blampos: bool,
 ) -> cp_model_pb2.CpSolverStatus:
     status, final_assignments = solve(
         men_count=men_count,
         women_count=women_count,
         courts_count=courts_count,
         matches_count=matches_count,
+        accommodate_blampos=accommodate_blampos,
     )
+
+    # Randomize court assignments within each time slot.
+    grouped_by_time_slot = collections.defaultdict(list)
+    for a in final_assignments:
+        grouped_by_time_slot[a.time_slot].append(a)
+    for assignments in grouped_by_time_slot.values():
+        random.shuffle(assignments)
+    shuffled_assignments: list[tuple[Assignment, int]] = [
+        (a, court) for assignments in grouped_by_time_slot.values()
+        for (a, court) in zip(assignments, range(1, courts_count + 1))
+    ]
+
     if status == cp_model.OPTIMAL:
         with open(filename, "w") as f:
             w = csv.writer(f)
             w.writerow(
-                ["Time slot", "Court", "Man A", "Man B", "Woman A", "Woman B"])
-            for fa in final_assignments:
+                ["Time slot", "Court", "Man A", "Woman A", "Man B", "Woman B"])
+            for fa, court in shuffled_assignments:
+                # Randomize team pairings within each quad.
                 men = list(fa.men)
                 women = list(fa.women)
                 random.shuffle(men)
                 random.shuffle(women)
-                w.writerow((fa.time_slot, fa.court, men[0], men[1], women[0],
-                            women[1]))
+
+                w.writerow(
+                    (fa.time_slot, court, men[0], women[0], men[1], women[1]))
 
     return status
 
@@ -198,31 +213,53 @@ def main():
                         type=string_to_two_int_tuple,
                         required=True)
     parser.add_argument("--courts_count", type=int, required=True)
+    parser.add_argument("--accommodate_blampos", type=bool, default=False)
 
     parsed = parser.parse_args()
 
     courts_count: int = parsed.courts_count
     min_max_men_count: tuple[int, int] = parsed.min_max_men_count
     min_max_women_count: tuple[int, int] = parsed.min_max_women_count
+    accommodate_blampos: bool = parsed.accommodate_blampos
+
+    if accommodate_blampos:
+        print('Accommodating Blampos')
+
+    combinations_tried: set[tuple[int, int]] = set()
 
     for men_count, women_count in itertools.product(
             range(min_max_men_count[0], min_max_men_count[1] + 1),
             range(min_max_women_count[0], min_max_women_count[1] + 1),
     ):
+        sorted_order = (men_count,
+                        women_count) if men_count <= women_count else (
+                            women_count, men_count)
+        if sorted_order in combinations_tried:
+            # Example: If we already calculated for 7 men and 4 women, skip
+            # for 4 men and 7 women because we can just switch the previous
+            # results.
+            continue
+        combinations_tried.add(sorted_order)
+
         print(
             f"Solving for {men_count} men, {women_count} women, {courts_count} courts."
         )
         min_matches_to_try = get_min_matches_to_try(men_count, women_count)
         solution_found = False
-        for matches_count in range(min_matches_to_try, min_matches_to_try * 5):
+        for matches_count in range(min_matches_to_try,
+                                   len(ALL_TIME_SLOTS) * courts_count + 1):
             print(f"Trying {matches_count} matches.")
+
+            blampos_name_part = 'helpingblampos' if accommodate_blampos else ''
+
             status = generate_csv(
                 filename=
-                f"{men_count}men_{women_count}women_{courts_count}courts.csv",
+                f"{men_count}men_{women_count}women_{courts_count}courts{blampos_name_part}.csv",
                 men_count=men_count,
                 women_count=women_count,
                 courts_count=courts_count,
                 matches_count=matches_count,
+                accommodate_blampos=accommodate_blampos,
             )
             if status == cp_model.OPTIMAL:
                 solution_found = True
